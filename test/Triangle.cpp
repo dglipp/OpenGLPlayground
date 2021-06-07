b1022b284b79a07d38885eb7cc808a9745d65c12
dglipp <dg.lippolis94@gmail.com>
2021-06-07 23:54:43 +0200

    tessellation

--- test/Triangle.cpp
+++ test/Triangle.cpp
@@ -16,11 +16,13 @@ class Triangle : public App
         {
             GLuint vertexShader;
             GLuint fragmentShader;
+            GLuint tessControlShader;
+            GLuint tessEvalShader;
             GLuint program;
 
             static const GLchar * vertSource[] = 
             {
-                "#version 450 core \n"
+                "#version 420 core \n"
                 " \n"\
                 "layout (location = 0) in vec4 offset; \n"
                 "layout (location = 1) in vec4 color; \n"
@@ -36,9 +38,40 @@ class Triangle : public App
                 "} \n"
             };
 
+            static const GLchar * tessControlSource[] =
+            {
+                "#version 420 core \n"
+                " \n"
+                "layout (vertices = 3) out; \n"
+                " \n"
+                "void main() \n"
+                "{ \n"
+                "if(gl_InvocationID == 0) \n"
+                "{ \n"
+                "gl_TessLevelInner[0] = 5.0; \n"
+                "gl_TessLevelOuter[0] = 5.0; \n"
+                "gl_TessLevelOuter[1] = 5.0; \n"
+                "gl_TessLevelOuter[2] = 5.0; \n"
+                "} \n"
+                "gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;"
+                "} \n"
+            };
+
+            static const GLchar * tessEvalSource[] = 
+            {
+                "#version 420 core \n"
+                " \n"
+                "layout (triangles, equal_spacing, cw) in; \n"
+                " \n"
+                "void main() \n"
+                "{ \n"
+                "gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position + gl_TessCoord.y * gl_in[1].gl_Position + gl_TessCoord.z * gl_in[2].gl_Position); \n"
+                "} \n"
+            };
+
             static const GLchar * fragSource[] = 
             {
-                "#version 450 core \n"
+                "#version 420 core \n"
                 " \n"
                 "in VS_OUT \n"
                 "{ \n"
@@ -55,16 +88,28 @@ class Triangle : public App
             glShaderSource(vertexShader, 1, vertSource, NULL);
             glCompileShader(vertexShader);
 
+            tessControlShader = glCreateShader(GL_TESS_CONTROL_SHADER);
+            glShaderSource(tessControlShader, 1, tessControlSource, NULL);
+            glCompileShader(tessControlShader);
+
+            tessEvalShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+            glShaderSource(tessEvalShader, 1, tessEvalSource, NULL);
+            glCompileShader(tessEvalShader);
+
             fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
             glShaderSource(fragmentShader, 1, fragSource, NULL);
             glCompileShader(fragmentShader);
 
             program = glCreateProgram();
             glAttachShader(program, vertexShader);
+            glAttachShader(program, tessControlShader);
+            glAttachShader(program, tessEvalShader);
             glAttachShader(program, fragmentShader);
             glLinkProgram(program);
 
             glDeleteShader(vertexShader);
+            glDeleteShader(tessControlShader);
+            glDeleteShader(tessEvalShader);
             glDeleteShader(fragmentShader);
 
             return program;
@@ -76,18 +121,19 @@ class Triangle : public App
             m_Program = compileShaders();
             glCreateVertexArrays(1, &m_VAO);
             glBindVertexArray(m_VAO);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         }
 
         void render(double time)
         {
-            const GLfloat clearColor[] = {0.0f, 0.0f, 0.3f, 0.0f};
+            const GLfloat clearColor[] = {1.0f, 1.0f, 1.0f, 0.0f};
             GLfloat color[] = {(float) std::abs(std::sin(time * 3)), (float) std::abs(std::sin(time * 5)), (float) std::abs(std::sin(time * 7)), 1.0f};
             GLfloat offset[] = {(float) std::sin(time * 3) * 0.5f, (float) std::cos(time * 5) * 0.5f, 0.0f, 0.0f};  
             glClearBufferfv(GL_COLOR, 0, clearColor);
             glUseProgram(m_Program);
             glVertexAttrib4fv(0, offset);
             glVertexAttrib4fv(1, color);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDrawArrays(GL_PATCHES, 0, 3);
         }
 
         void shutdown()
