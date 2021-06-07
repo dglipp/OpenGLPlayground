f06b451bd8c9f9ed3a0f4e9bac540c4ecfc3d36f
dglipp <dg.lippolis94@gmail.com>
2021-06-07 21:41:24 +0200

    make triangle move\'
    f

--- test/Triangle.cpp
+++ test/Triangle.cpp
@@ -21,11 +21,12 @@ class Triangle : public App
             static const GLchar * vertSource[] = 
             {
                 "#version 450 core \n"
-                " \n"
+                " \n"\
+                "layout (location = 0) in vec4 offset;"
                 "void main() \n"
                 "{ \n"
                 "const vec4 vertices[3] = vec4[3](vec4(0.25, -0.25, 0.5, 1.0), vec4(-0.25, -0.25, 0.5, 1.0), vec4(0.25, 0.25, 0.5, 1.0)); \n"
-                "gl_Position = vertices[gl_VertexID]; \n"
+                "gl_Position = vertices[gl_VertexID] + offset; \n"
                 "} \n"
             };
 
@@ -70,8 +71,10 @@ class Triangle : public App
         void render(double time)
         {
             const GLfloat color[] = {0.0f, 0.0f, 0.3f, 0.0f};
+            GLfloat offset[] = {(float) std::sin(time * 3) * 0.5f, (float) std::cos(time * 5) * 0.5f, 0.0f, 0.0f};  
             glClearBufferfv(GL_COLOR, 0, color);
             glUseProgram(m_Program);
+            glVertexAttrib4fv(0, offset);
             glDrawArrays(GL_TRIANGLES, 0, 3);
         }
 
