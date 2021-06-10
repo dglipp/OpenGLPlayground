4b449980756684a8d17df807a4317c1ff19685fb
dglipp <dg.lippolis94@gmail.com>
2021-06-10 14:14:40 +0200

    fixed bug that prevented rendering with mesa drivers

--- test/Triangle.cpp
+++ test/Triangle.cpp
@@ -24,28 +24,24 @@ class Triangle : public App
             static const GLchar * vertSource[] = 
             {
                 "#version 420 core \n"
-                " \n"\
                 "layout (location = 0) in vec4 offset; \n"
                 "layout (location = 1) in vec4 color; \n"
-                "out VS_OUT \n"
-                "{ \n"
-                "vec4 color; \n"
-                "} vs_out; \n"
+                "out vec4 vs_color; \n"
                 "void main() \n"
                 "{ \n"
                 "const vec4 vertices[3] = vec4[3](vec4(-1.0, -1.0 , 1.0, 1.0), vec4(1.0, -1.0, 1.0, 1.0), vec4(0.0, 1.0, 1.0, 1.0)); \n"
                 "gl_Position = vertices[gl_VertexID] + offset; \n"
-                "vs_out.color = color; \n"
+                "vs_color = color; \n"
                 "} \n"
             };
 
             static const GLchar * tessControlSource[] =
             {
                 "#version 420 core \n"
-                " \n"
                 "layout (vertices = 3) out; \n"
                 "uniform vec4 tessGran; \n"
-                " \n"
+                "in vec4 vs_color[]; \n"
+                "out vec4 tcs_color[]; \n"
                 "void main() \n"
                 "{ \n"
                 "if(gl_InvocationID == 0) \n"
@@ -55,54 +51,62 @@ class Triangle : public App
                 "gl_TessLevelOuter[1] = tessGran[2]; \n"
                 "gl_TessLevelOuter[2] = tessGran[3]; \n"
                 "} \n"
-                "gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;"
+                "tcs_color[gl_InvocationID] = vs_color[gl_InvocationID]; \n"
+                "gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position; \n"
                 "} \n"
             };
 
             static const GLchar * tessEvalSource[] = 
             {
                 "#version 420 core \n"
-                " \n"
                 "layout (triangles, equal_spacing, cw) in; \n"
                 "uniform vec3 mover; \n"
-                " \n"
+                "in vec4 tcs_color[]; \n"
+                "out vec4 tes_color; \n"
                 "void main() \n"
                 "{ \n"
                 "gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position*mover[0] + gl_TessCoord.y * gl_in[1].gl_Position*mover[1] + gl_TessCoord.z * gl_in[2].gl_Position*mover[2]); \n"
+                "tes_color = (gl_TessCoord.x * tcs_color[0] + gl_TessCoord.y * tcs_color[1] + gl_TessCoord.z * tcs_color[2]); \n"
                 "} \n"
             };
 
             static const GLchar * fragSource[] = 
             {
                 "#version 420 core \n"
-                " \n"
-                "in VS_OUT \n"
-                "{ \n"
-                "vec4 color; \n"
-                "} fs_in; \n"
+                "in vec4 tes_color; \n"
                 "out vec4 color; \n"
                 "void main() \n"
                 "{ \n"
-                "color = fs_in.color; \n"
+                "color = tes_color; \n"
                 "} \n"
             };
+
+            GLchar message[1024];
 
             vertexShader = glCreateShader(GL_VERTEX_SHADER);
             glShaderSource(vertexShader, 1, vertSource, NULL);
             glCompileShader(vertexShader);
+            glGetShaderInfoLog(vertexShader, 1024, nullptr, message);
+            std::cout << "VERTEX: " << message << std::endl;
 
             tessControlShader = glCreateShader(GL_TESS_CONTROL_SHADER);
             glShaderSource(tessControlShader, 1, tessControlSource, NULL);
             glCompileShader(tessControlShader);
+            glGetShaderInfoLog(tessControlShader, 1024, nullptr, message);
+            std::cout << "TCS: "<< message << std::endl;
 
             tessEvalShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
             glShaderSource(tessEvalShader, 1, tessEvalSource, NULL);
             glCompileShader(tessEvalShader);
+            glGetShaderInfoLog(tessEvalShader, 1024, nullptr, message);
+            std::cout << "TES: "<< message << std::endl;
 
             fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
             glShaderSource(fragmentShader, 1, fragSource, NULL);
             glCompileShader(fragmentShader);
-
+            glGetShaderInfoLog(fragmentShader, 1024, nullptr, message);
+            std::cout << "FRAGMENT: "<< message << std::endl;
+            
             program = glCreateProgram();
             glAttachShader(program, vertexShader);
             glAttachShader(program, tessControlShader);
@@ -114,7 +118,8 @@ class Triangle : public App
             glDeleteShader(tessControlShader);
             glDeleteShader(tessEvalShader);
             glDeleteShader(fragmentShader);
-
+            glGetProgramInfoLog(program, 1024, nullptr, message);
+            std::cout << "PROGRAM: "<< message << std::endl;
             return program;
         }
 
@@ -131,7 +136,7 @@ class Triangle : public App
         {
             if(m_tessGran[1] == 30.0f) m_tessGran[2] = +2.0f;
             if(m_tessGran[1] == 70.0f) m_tessGran[2] = -2.0f;
-            const GLfloat clearColor[] = {1.0f, 1.0f, 1.0f, 0.0f};
+            const GLfloat clearColor[] = {0.0f, 0.0f, 0.0f, 0.0f};
             GLfloat color[] = {(float) std::abs(std::sin(time * 3)), (float) std::abs(std::sin(time * 5)), (float) std::abs(std::sin(time * 7)), 1.0f};
             GLfloat offset[] = {0.0f, 0.0f, 0.0f, 0.0f};  
             glClearBufferfv(GL_COLOR, 0, clearColor);
