88bde4b4ee09e603d996c15a1cb1d9253c3cd6e9
dglipp <dg.lippolis94@gmail.com>
2021-11-02 13:01:37 +0100

    Completed proto light classes

--- include/INTERNAL/Light.h
+++ include/INTERNAL/Light.h
@@ -42,5 +42,54 @@ namespace light
 
     class Positional : public Ambient
     {
+        protected:
+            glm::vec3 m_Location;
+            float m_Kc;
+            float m_Kl;
+            float m_Kq;
+
+        public:
+            Positional();
+            Positional(glm::vec4 ambientLight, glm::vec4 diffuseLight, glm::vec4 specularLight,
+                       glm::vec3 location = glm::vec3(0.0f, 0.0f, 0.0f),
+                       float Kc = 1.2f, float Kl = 0.2f, float Kq = 0.0f);
+
+            glm::vec4 getDiffuse();
+            glm::vec4 getSpecular();
+            glm::vec3 getLocation();
+            float getKc();
+            float getKl();
+            float getKq();
+
+            void setDiffuse(glm::vec4 diffuseLight);
+            void setSpecular(glm::vec4 specularLight);
+            void setLocation(glm::vec3 direction);
+            void setKc(float Kc);
+            void setKl(float Kl);
+            void setKq(float Kq);
+
+            float getDistanceAttenuation(glm::vec3 targetPosition);
+    };
+
+    class Spotlight : public Positional, public Directional
+    {
+        protected:
+            float m_Cutoff;
+            float m_Exponent;
+
+        public:
+            Spotlight();
+            Spotlight(glm::vec4 ambientLight, glm::vec4 diffuseLight, glm::vec4 specularLight,
+                      glm::vec3 location = glm::vec3(0.0f, 0.0f, 0.0f),
+                      glm::vec3 direction = glm::vec3(0.0f, 0.0f, -1.0f),
+                      float Kc = 1.2f, float Kl = 0.2f, float Kq = 0.0f, float cutoff = 20.0f, float exponent = 10.0f);
+
+            float getCutoff();
+            float getExponent();
+
+            void setCutoff(float cutoff);
+            void setExponent(float exponent);
+
+            float getSpotlightAttenuation(glm::vec3 targetPosition);
     };
 }
--- test/LightTest.cpp
+++ test/LightTest.cpp
@@ -6,8 +6,5 @@
 
 int main()
 {
-    light::Ambient amb(glm::vec4(0.8, 0.2, 0.1, 1.0));
-    light::Directional dir(glm::vec4(0.8, 0.2, 0.1, 1.0), glm::vec4(0.8, 0.2, 0.8, 1.0), glm::vec4(0.1, 0.1, 0.1, 1.0));
-
-    std::cout << dir.getDiffuse().y << '\n';
+    
 }
